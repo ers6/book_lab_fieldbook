fddf85417dc92c7ee251de7bb86ba5c3777e5dae
ers6 <98556588+ers6@users.noreply.github.com>
2022-06-18 12:13:38 -0500

    Update scrapes_funding_data.py

--- scrapes_funding_data.py
+++ scrapes_funding_data.py
@@ -16,9 +16,31 @@ def main():
     makes_results_csv(these_results, "results.csv")
 
 
+# deprecated version of gets funding data that returns results as a list of strings instead of a massive string 
+# def gets_funding_data(infile_name, search_term):
+#     #need a try/ except block here 
+#     infile = open(infile_name, 'r')
+#     # turn the whole thing into one big list
+#     lines = infile.read().splitlines()
+#     i = 0
+#     hits = []
+#     results = []
+#     for line in lines:
+#         if search_term.lower() in line.lower():
+#             print('found in line:', i, line)
+#             hits.append(i)
+#         i += 1
+#     print(hits)
+#     for hit in hits:
+#         print(lines[(hit - 5):(hit + 20)])
+#         result = (lines[(hit - 5):(hit + 20)])
+#         results.append({'volume': infile_name, 'line' : hit, 'text':result})
+#     headers = ['volume','line', 'text']
+#     rows = results
+#     return rows
+
 
 def gets_funding_data(infile_name, search_term):
-    #need a try/ except block here 
     infile = open(infile_name, 'r')
     # turn the whole thing into one big list
     lines = infile.read().splitlines()
@@ -32,12 +54,17 @@ def gets_funding_data(infile_name, search_term):
         i += 1
     print(hits)
     for hit in hits:
-        print(lines[(hit - 5):(hit + 20)])
-        result = (lines[(hit - 5):(hit + 20)])
-        results.append({'volume': infile_name, 'line' : hit, 'text':result})
+        print(lines[(hit - 10):(hit + 10)])
+        result = (lines[(hit - 5):(hit + 5)])
+        result_string = ""
+        for item in result:
+            result_string = result_string + " " + item
+        result_string = re.sub("\\\\", "", result_string)
+        results.append({'volume': infile_name, 'line' : hit, 'text':result_string})
     headers = ['volume','line', 'text']
     rows = results
     return rows
+
 
 
 def makes_results_csv(results, outfile_name):
